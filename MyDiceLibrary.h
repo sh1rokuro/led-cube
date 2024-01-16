363c0ecf851fca89801a6957620f2d69f2f1975d
sh1rokuro <148336300+sh1rokuro@users.noreply.github.com>
2024-01-16 07:53:27 +0100

    Add files via upload

---
+++ MyDiceLibrary.h
@@ -0,0 +1,36 @@
+#ifndef MyDiceLibrary_h
+#define MyDiceLibrary_h
+
+#include "Arduino.h"
+
+class MyDiceLibrary {
+public:
+  static int bottomLeft;
+  static int middleLeft;
+  static int upperLeft;
+  static int middle;
+  static int bottomRight;
+  static int middleRight;
+  static int upperRight;
+
+  static bool initialAnimationDone;
+
+  static void enterSleepMode();
+  static void setupWatchdog(int time);
+
+  static void buttonInterrupt();
+  static void clear();
+  static void one();
+  static void two();
+  static void three();
+  static void four();
+  static void five();
+  static void six();
+  static void simpleAnimation();
+  static void RollingDiceAnimation();
+  
+private:
+  static byte getWDTControl(int time);
+};
+
+#endif
